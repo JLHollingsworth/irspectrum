8d856ecb4d739b4386e9e1ce8c6e474487fa3cc5
joshuaellis555 <joshuaellis101@gmail.com>
2018-01-24 18:59:13 -0600

    Update IR_read_graph.py

--- IR_read_graph.py
+++ IR_read_graph.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageTk
 import sys
 
+#open image from user's commandline argument
 img = Image.open(sys.argv[1])
 imgdata=list(img.getdata())#the pixels from the image
 
@@ -8,64 +9,59 @@ imgdata=list(img.getdata())#the pixels from the image
 Width=1024
 Height=768
 
-#the area of each image that we want (the graph)
+#the area of each image that we want (the actual graph)
         #(left,right,top,bottom)
 targetRect=(113,978,29,724)
 
-def copyRect(source,rect):#copies pixels from the source image within the targetRect
+#copies pixels from the source image within the targetRect
+def cropRect(source,rect):
     left,right,top,bottom=rect
-    if left<0: left=0
-    if top<0: top=0
-    if right>=Width: right=Width-1
-    if bottom>=Height: bottom=Height-1
-
     newImg=[]
-    
+    #copy specified pixels
     for y in range(top,bottom+1):
         for x in range(left,right+1):
             newImg+=[source[y*Width+x]]
     return newImg
 
 #the graph cut out of the larger image
-graph=copyRect(imgdata,targetRect)
+graph=cropRect(imgdata,targetRect)
 
-#width and height of out cropped graph
+#width and height of cropped graph
 Width=targetRect[1]-targetRect[0]+1
 Height=targetRect[3]-targetRect[2]+1
 
-#save the graph image
+#save the cropped graph image
 img = Image.new('RGB', (Width, Height))
 img.putdata(graph)
 img.save('graph.png')
 
-data=[]
-
 def pix(x,y):#checks if the pixel at x,y is black
-    if x>=Width or y>=Height or x<0 or y<0:
-        print('Out of range!:',x,y,'\n')
+    r,g,b=graph[y*Width+x]
+    if r+g+b>=100:
+        return False#not black
     else:
-        r,g,b=graph[y*Width+x]
-        if r+g+b>=100:
-            return False
-        else:
-            return True
+        return True#black
 
-#for each x get the range over which the graph is drawn (has black pixels)
-    # or None if the graph is empty at that x value
+data=[]#to be filled with values from graph
+
+#For each x get the y range over which the graph has black pixels
+# or None if the graph is empty at that x value
 for x in range(0,Width):
     data+=[None]
-    foundPix=False
+    foundPix=False#have you found a pixel while looping through the column
     for y in range(0,Height):
-        p=pix(x,y)
+        p=pix(x,y)#is the pixel black
         if p and not foundPix:
+            #record the first black pixels y value
             foundPix=True
             maxVal=y
         elif not p and foundPix:
+            #record the last black pixels y value
             minVal=y
-            data[-1]=(minVal,maxVal)
-            break
+            data[-1]=(minVal,maxVal)#write these values to data
+            break#next x
 
-#save data
+#save data to file
 f = open("data.txt", "w")
 for element in data:
     f.write(str(element) + '\n')
